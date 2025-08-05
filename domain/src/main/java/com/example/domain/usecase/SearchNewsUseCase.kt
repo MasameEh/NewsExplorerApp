50fbdf0bec6ec58ac56d96c0da6480e7e911a07e
MasameEh <samehms116@gmail.com>
2025-08-05 21:10:35 +0300

    sort news asc by publish date

--- domain/src/main/java/com/example/domain/usecase/SearchNewsUseCase.kt
+++ domain/src/main/java/com/example/domain/usecase/SearchNewsUseCase.kt
@@ -1,15 +1,30 @@
 package com.example.domain.usecase
 
+import android.os.Build
+import androidx.annotation.RequiresApi
 import com.example.domain.adapters.INewsRepository
 import com.example.domain.entities.News
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.flow
+import java.time.ZonedDateTime
+import java.time.format.DateTimeFormatter
 import javax.inject.Inject
 
-class SearchNewsUseCase @Inject constructor(private val newsRepository: INewsRepository) {
+class SearchNewsUseCase @Inject constructor(
+    private val newsRepository: INewsRepository
+) {
+    @RequiresApi(Build.VERSION_CODES.O)
+    operator fun invoke(query: String): Flow<List<News>> = flow {
+        val newsList = newsRepository.searchNews(query).news
 
-     operator fun invoke(query: String): Flow<List<News>> = flow {
-         emit(newsRepository.searchNews(query).news)
+        val sortedList = newsList.sortedByDescending { news ->
+            runCatching {
+                ZonedDateTime.parse(news.publishedAt, DateTimeFormatter.ISO_DATE_TIME)
+            }.getOrElse {
+                ZonedDateTime.now().minusYears(100)
+            }
+        }
+
+        emit(sortedList)
     }
-
 }
